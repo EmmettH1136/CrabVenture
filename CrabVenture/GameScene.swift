9444b675933b349e85e10024dcf0962ec2e246c4
mikey <mlinker@stu.d214.org>
2019-05-01 11:27:01 -0500

    Added physics body to crab Claw and added to viewController

--- CrabVenture/GameScene.swift
+++ CrabVenture/GameScene.swift
@@ -15,6 +15,7 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         var crabClaw = SKSpriteNode()
         crabClaw = self.childNode(withName: "SKSpriteNode") as! SKSpriteNode
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
