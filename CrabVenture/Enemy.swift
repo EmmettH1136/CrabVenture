e99a414046ab96074be9a8c3f7d934d88b3ca448
Emmett <Ehasley1136@stu.d214.org>
2019-05-07 11:35:20 -0500

    Why\

--- CrabVenture/Enemy.swift
+++ CrabVenture/Enemy.swift
@@ -15,12 +15,14 @@ class Enemy {
 	var speed : CGVector
 	var health : Int
 	var body : SKSpriteNode
+	var z : Int
 	
-	init (_ name : String, _ health : Int, _ body: SKSpriteNode, _ speed : CGVector) {
+	init (_ name : String, _ health : Int, _ body: SKSpriteNode, _ speed : CGVector, _ z: Int) {
 		self.name = name
 		self.speed = speed
 		self.health = health
 		self.body = body
+		self.z = z
 		
 	}
 	
