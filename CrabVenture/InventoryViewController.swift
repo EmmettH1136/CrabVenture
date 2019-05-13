c2fd7d9cbc01100992491c55eb6edc5f6a9ef39b
mikey <mlinker@stu.d214.org>
2019-05-13 11:13:53 -0500

    Added modular View Controller

---
+++ CrabVenture/InventoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InventoryViewController.swift
+//  CrabVenture
+//
+//  Created by Michael Linker on 5/13/19.
+//  Copyright © 2019 John Heresy High School. All rights reserved.
+//
+
+import UIKit
+
+class InventoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
